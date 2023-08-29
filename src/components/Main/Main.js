0a93838b343d80897ce33c93432180b8393def1b
Juliya Koltsova <juliyaasking009@gmail.com>
2023-08-29 21:53:26 +0200

    feat: портирована вёрстка и создан стили для promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 import './Main.css';
 
+import Promo from './Promo/Promo';
+import AboutProject from './AboutProject/AboutProject';
+import Techs from './Techs/Techs';
+import AboutMe from './AboutMe/AboutMe';
+import Portfolio from './Portfolio/Portfolio';
+
 function Main(props) {
   return(
-    <main className="content"></main>
+    <main className="content">
+      <Promo />
+      <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+    </main>
   )
 }
 
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import logoPromo from '../../../images/landing-logo.png';
+
 import './Promo.css';
 
-function Promo(props) {}
+function Promo(props) {
+  return(
+    <section className="promo section-container">
+      <div className="promo__content">
+        <h1 className="propmo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      <button type="button" className="promo__more-button link-hover">Узнать больше</button>
+      </div>
+      <img src={logoPromo} className="promo__logo" alt="Логотип промо секции" />
+    </section>
+  );
+}
 
 export default Promo;
