a06126018935aa75a4b0ef2b61c1cac91cdbc922
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-05 16:05:26 +0200

    fix: исправлено отображение шапки на разных страницах

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,31 +18,31 @@ function Header(props) {
       <li className="menu__item menu__item_rigth-menu">
         <ul className="menu-rigth__menu-list">
           {location.pathname !== '/' && <li className="menu__item"><NavLink to="/" className={({isActive}) => `menu__link ${isActive ? "menu__link_active" : ""}`}>Фильмы</NavLink></li>}
-          <li className="menu__item"><NavLink to="/movies" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""}`}>Фильмы</NavLink></li>
-          <li className="menu__item"><NavLink to="/saved-movies" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""}`}>Сохранённые фильмы</NavLink></li>
+          <li className="menu__item"><NavLink to="/movies" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""} ${location.pathname === '/' ? "menu__link_color_white" : "menu__link_color_black"}`}>Фильмы</NavLink></li>
+          <li className="menu__item"><NavLink to="/saved-movies" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""} ${location.pathname === '/' ? "menu__link_color_white" : "menu__link_color_black"}`}>Сохранённые фильмы</NavLink></li>
         </ul>
       </li>
-      <li className="menu__item menu__item_profile link-hover"><NavLink to="/profile" className={({isActive}) => `menu__link ${isActive ? "menu__link_active" : ""}`}>Аккаунт</NavLink></li>
+      <li className="menu__item menu__item_profile link-hover"><NavLink to="/profile" className={({isActive}) => `menu__link ${isActive ? "menu__link_active" : ""} ${location.pathname === '/' ? "menu__link_color_white" : "menu__link_color_black"}`}>Аккаунт</NavLink></li>
     </ul>
   );
 
   const isNotLoggedNav = (
-    <ul className="menu__menu-list menu__menu-list_isNotLogged">
-      <li className="menu__item"><NavLink to="/signup" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""}`}>Регистрация</NavLink></li>
-      <li className="menu__item"><NavLink to="/signin" className={({isActive}) => `menu__link menu__link_login-button link-hover ${isActive ? "menu__link_active" : ""}`}>Войти</NavLink></li>
+    <ul className={`menu__menu-list menu__menu-list_isNotLogged`}>
+      <li className="menu__item"><NavLink to="/signup" className={({isActive}) => `menu__link link-hover ${isActive ? "menu__link_active" : ""} ${location.pathname === '/' ? "menu__link_color_white" : "menu__link_color_black"}`}>Регистрация</NavLink></li>
+      <li className="menu__item"><NavLink to="/signin" className={({isActive}) => `menu__link menu__link_login-button link-hover ${isActive ? "menu__link_active" : ""} ${location.pathname === '/' ? "menu__link_color_white" : "menu__link_color_black"}`}>Войти</NavLink></li>
     </ul>
   );
 
   return(
-    <header className={`header section-container ${location.pathname === '/' ? "header_theme_blue" : "header_theme_white"}  ${isMobileMenuOpen === true ? "header_mobile" : ""}`}>
+    <header className={`header section-container ${location.pathname === '/' ? "header_theme_blue" : "header_theme_white"} ${isMobileMenuOpen === true ? "header_mobile" : ""}`}>
       <NavLink className="header__logo-link link-hover" to={"/"}>
       <img className='header__logo' src={logo} alt="Логотип проекта 'Movies Explorer'" />
       </NavLink>
-      <div className={`header__burger-menu link-hover ${isMobileMenuOpen ? "header__burger-menu_active" : ""}`} onClick={toggleMenu}>
+      <div className={`header__burger-menu link-hover ${!isLoggedIn ? "header__burger-menu_display_none" : ""} ${isMobileMenuOpen ? "header__burger-menu_active" : ""}`} onClick={toggleMenu}>
         <span></span>
       </div>
 
-      <nav className={`menu ${isMobileMenuOpen ? "menu_mobile-menu-active" : ""}`}>
+      <nav className={`menu ${isMobileMenuOpen ? "menu_mobile-menu-active" : ""} ${isLoggedIn ? "menu_isLogged" : ""}`}>
         {isLoggedIn ? isLoggedNav : isNotLoggedNav}
       </nav>
     </header>
