96775b9b32bc1002ccf68dfb0094ff35e0d48ac2
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-07 18:46:41 +0200

    fix: править стили form для 320px

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -7,7 +7,7 @@ function Input(props) {
     <div className={`form__input form__input_type_${props.formName}`}>
       <label className={`input__label input__label_type_${props.formName}`} for={props.id}>{props.label}</label>
       <input type={props.type} className={`input input_type_${props.formName}`} name={props.name} id={props.id} placeholder={props.placeholder} minLength={props.min} maxLength={props.max} required />
-      <span className={`input__input-error ${props.name}-error input__input-error_type_${props.formName}`}>eee</span>
+      <span className={`input__input-error ${props.name}-error input__input-error_type_${props.formName}`}></span>
     </div>
   );
 }
