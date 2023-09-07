3691e29a7d35b5fb70e6f6a4a9c7c96b22bc468a
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-07 04:04:09 +0200

    fix: править стили 404 под 768px

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -6,13 +6,11 @@ import './NotFoundPage.css';
 function PageNotFound() {
   return(
     <main className="not-found-page">
-      <div className="not-found-page__container">
       <h2 className="not-found-page__title">
         <span className="not-found-page__err-code">404</span>
         <p className="not-found-page__err-message">Страница не найдена</p>
       </h2>
       <Link to="/" className="button button_type_to-main link-hover">Назад</Link>
-      </div>
     </main>
   );
 }
