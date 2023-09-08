f23c11042efab19b0283db73251650c6e0e4d7cb
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-09 00:50:20 +0200

    fix: временно добавить возможность встроенной браузерной валидации поле форм

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../Logo/Logo';
@@ -6,14 +6,18 @@ import Button from '../Button/Button';
 import './Form.css';
 
 function Form(props) {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return(
     <section className="section-form">
       <Logo />
-      <form action="" className={`form form_type_${props.name}`} name={props.name} noValidate={true}>
+      <form action="" className={`form form_type_${props.name}`} name={props.name} onSubmit={handleSubmit}>
         <h2 className="form__title">{props.title}</h2>
         {props.children}
         <div className="form__bottom">
-          <Button type="submit" className="button_type_form-submit" buttonText={props.buttonText} />
+          <Button type="submit" className="button_type_form-submit" buttonText={props.buttonText}  />
           <p className={`form__footer form__footer_type_${props.name}`}>{props.text}<Link to={props.endpoint} className="form__footer-link link-hover">{props.linkText}</Link></p>
         </div>
       </form>
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -4,11 +4,11 @@ import './Input.css';
 
 function Input(props) {
   return(
-    <div className={`form__input form__input_type_${props.formName}`}>
-      <label className={`input__label input__label_type_${props.formName}`} for={props.id}>{props.label}</label>
+    <>
+      <label className={`input__label input__label_type_${props.formName}`} htmlFor={props.id}>{props.label}</label>
       <input type={props.type} className={`input input_type_${props.formName}`} name={props.name} id={props.id} placeholder={props.placeholder} minLength={props.min} maxLength={props.max} required />
       <span className={`input__input-error ${props.name}-error input__input-error_type_${props.formName}`}></span>
-    </div>
+    </>
   );
 }
 
