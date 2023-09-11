391f9291215b7670f41bc16e91417edd1205773f
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-11 23:14:04 +0200

    fix: редактировать названия классов в соответствии с бэм

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -8,17 +8,17 @@ function AboutProject(props) {
   return(
     <section className="about-project section-container" id="about-project" aria-label="О проекте">
       <Title title="О проекте" />
-      <div className="about-project__description">
+      <div className="about-project__description project-description">
         <article className="project-description__column">
-          <h2 className="description__title">Дипломный проект включал 5 этапов</h2>
-          <p className="description__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <h2 className="project-description__title">Дипломный проект включал 5 этапов</h2>
+          <p className="project-description__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </article>
         <article className="project-description__column">
-        <h2 className="description__title">На выполнение диплома ушло 5 недель</h2>
-          <p className="description__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <h2 className="project-description__title">На выполнение диплома ушло 5 недель</h2>
+          <p className="project-description__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </article>
       </div>
-      <table className="about-ptoject__timeline-table">
+      <table className="about-project__timeline-table timeline-table">
         <tr className="timeline-table__table-row timeline-table__table-row_time">
           <td className="timeline-table__item timeline-table__item_first-time">1 неделя</td>
           <td className="timeline-table__item">4 недели</td>
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -9,9 +9,9 @@ function Promo(props) {
   return(
     <section className="promo section-container">
       <div className="promo__content">
-        <h1 className="propmo__title">Учебный проект студента факультета Веб-разработки.</h1>
-        <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <Link to="about-project" spy={true} smooth={true} className="promo__learn-more-button button-hover">Узнать больше</Link>
+        <h1 className="promo__content-title">Учебный проект студента факультета Веб-разработки.</h1>
+        <p className="promo__content-subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        <Link to="about-project" spy={true} smooth={true} className="promo__content-learn-more-button button-hover">Узнать больше</Link>
       </div>
       <img src={logoPromo} className="promo__logo" alt="Логотип промо секции" />
     </section>
