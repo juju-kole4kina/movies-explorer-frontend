fac39a0931dc9fb511c78850d2494d635f5cbd8a
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-12 16:28:55 +0200

    fix: добавить инпут

---
+++ src/components/Input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Input.css';
+
+function Input(props) {
+  return(
+    <>
+      <label className={`${props.formName}-input-list__item input-label input-label_type_${props.formName}`} htmlFor={props.id}>{props.label}</label>
+      <input type={props.type} className={`${props.formName}-input-list__item input input_type_${props.formName}`} name={props.name} id={props.id} placeholder={props.placeholder} minLength={props.min} maxLength={props.max} required />
+      <span className={`${props.formName}-input-list__item input-error ${props.name}-error input-error_type_${props.formName}`}></span>
+    </>
+  );
+}
+
+export default Input;
