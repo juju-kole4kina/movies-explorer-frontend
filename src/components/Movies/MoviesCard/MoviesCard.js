be304764890f3ef9d49458f0d8b5bcb856b216a7
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-12 04:08:22 +0200

    fix: заменить button на компонент Button

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import Button from '../../Button/Button';
 
 import './MoviesCard.css';
 
@@ -19,7 +20,7 @@ function MoviesCard(props) {
           <h3 className="movie-card__title">{props.cardName}</h3>
           <p className="movie-card__movie-timeline">{props.timeline}</p>
         </div>
-        <button type="button" onClick={toggleSaveButton} className={`button button_type_save-card ${isSave ? "button_type_save-card-active" : ""} ${location.pathname === '/saved-movies' ? "button_type_delete-card" : ""} button-hover`}></button>
+        <Button type="button" onClick={toggleSaveButton} className={`button_type_save-card ${isSave ? "button_type_save-card-active" : ""} ${location.pathname === '/saved-movies' ? "button_type_delete-card" : ""}`} />
       </div>
       <Link to={props.link} className="movie-card__link" target="_blank">
       <img className="movie-card__image" alt={props.alt} src={props.img} />
