b4617a5a2d5b60ac798fbeb24cd75a0ce7900d21
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-14 17:11:25 +0200

    fix: добавлен семантический тег section для 404

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -13,12 +13,14 @@ function PageNotFound() {
 
   return(
     <main className="not-found-page">
-      <h2 className="not-found-page__title">
-        <span className="not-found-page__err-code">404</span>
-        <p className="not-found-page__err-message">Страница не найдена</p>
-      </h2>
-      <Button onClick={handleGoBack} className="button_type_go-back" buttonText="Назад" />
-    </main>
+      <section className="not-found-page__section">
+        <h2 className="not-found-page__title">
+          <span className="not-found-page__err-code">404</span>
+          <p className="not-found-page__err-message">Страница не найдена</p>
+        </h2>
+        <Button onClick={handleGoBack} className="button_type_go-back" buttonText="Назад" />
+      </section>
+      </main>
   );
 }
 
