782cc069504340f6a68816a3350a7dac94a93324
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-14 16:26:20 +0200

    fix: исправить работу ссылок в footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,8 +13,8 @@ function Footer(props) {
       <div className="footer__row row-bottom">
         <p className="footer__copyrigth">© 2020</p>
         <ul className="footer__link-list">
-          <li className="footer__link-item link-hover"><Link to="https://practicum.yandex.ru/" className="footer__link" target="_blank">Яндекс.Практикум</Link></li>
-          <li className="footer__link-item link-hover"><Link to="https://github.com/topics/yandex-praktikum" className="footer__link" target="_blank">Github</Link></li>
+          <li className="footer__link-item link-hover"><a href="https://practicum.yandex.ru/" className="footer__link" target="_blank">Яндекс.Практикум</a></li>
+          <li className="footer__link-item link-hover"><a href="https://github.com/topics/yandex-praktikum" className="footer__link" target="_blank">Github</a></li>
         </ul>
       </div>
     </footer>
