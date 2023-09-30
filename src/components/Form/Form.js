92b8550e81117d9b098354ea361f6de5bfbcae90
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-30 21:19:20 +0200

    fix: сделаны правки в компоненте form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,20 +6,31 @@ import Button from '../Button/Button';
 import './Form.css';
 
 function Form(props) {
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
   return(
     <section className="section-form">
       <Logo />
-      <form className={`form form-input-list form_type_${props.name}`} name={props.name} onSubmit={handleSubmit}>
+      <form
+        className={`form form-input-list form_type_${props.name}`}
+        name={props.name}
+        onSubmit={props.onSubmit}
+        noValidate={true}
+      >
         <h2 className="form__title">{props.title}</h2>
         {props.children}
         <div className="form__bottom">
         <span className="form__error-message">{props.errorMessage || ''}</span>
-          <Button type="submit" className={`button_type_form-submit ${props.isValid === false ? "button_type_disabled" : "button-hover"}`} buttonText={props.buttonText} disabled={props.disabled} />
-          <p className={`form__footer form__footer_type_${props.name}`}>{props.text}<Link to={props.endpoint} className="form__footer-link link-hover">{props.linkText}</Link></p>
+          <Button
+            type="submit"
+            className={`button_type_form-submit ${props.isValid === false ? "button_type_disabled" : "button-hover"}`}
+            buttonText={props.buttonText}
+            disabled={props.disabled}
+          />
+          <p className={`form__footer form__footer_type_${props.name}`}>
+            {props.text}
+            <Link to={props.endpoint} className="form__footer-link link-hover">
+              {props.linkText}
+            </Link>
+          </p>
         </div>
       </form>
     </section>
