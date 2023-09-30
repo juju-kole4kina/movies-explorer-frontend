c113feaa78f3684a39169540ebd37af307ea9b0a
Juliya Koltsova <juliyaasking009@gmail.com>
2023-09-30 22:24:26 +0200

    fix: правки в компоненте moviesCardLis

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import movieList from '../../../utils/movieList';
-import NoResult from '../NoResult/NoResult';
 import Button from '../../Button/Button';
 import './MoviesCardList.css';
 
 function MoviesCardList(props) {
   const location = useLocation();
 
-  function handleShowMore() {}
   return(
-    <section className="movie-list-section section-container" aria-label="Список фиьмов">
-      {movieList.length > 0 ?
-      (<>
+    <section
+      className="movie-list-section section-container"
+      aria-label="Список фиьмов"
+    >
       <ul className="movie-list">
         {props.children}
       </ul>
-      <Button type="button" className={`button_type_more ${(location.pathname === '/saved-movies' || props.hiddenMovies.length === 0) ? "button_type_more-disabled" : ""}`} onClick={handleShowMore} buttonText="Ещё" />
-      </>) :
-      (<NoResult />)}
+      <Button
+        type="button"
+        className={`button_type_more ${
+          (location.pathname === '/saved-movies' ||
+            props.hiddenMovies.length === 0)
+            ? "button_type_more-disabled" : ""
+          }`}
+          onClick={props.handleShowMore}
+          buttonText="Ещё"
+        />
     </section>
   );
 }
